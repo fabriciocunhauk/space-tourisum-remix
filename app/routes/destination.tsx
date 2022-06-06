b04cf95bd3a9c2c0a4f29820ae1ae556ed9f1975
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2022-06-06 18:43:41 +0100

    Changes to destination links

--- app/routes/destination.tsx
+++ app/routes/destination.tsx
@@ -1,26 +1,27 @@
 import LayoutContainer from "~/components/LayoutContainer/LayoutContainer";
 import background from "../../public/assets/destination/background-destination-desktop.jpg";
 import { Link, Outlet } from "@remix-run/react";
+import { planetsData } from "../../planetsData";
 
 const destination = () => {
   return (
     <LayoutContainer image={background}>
-      <div className="w-screen h-1/2 flex flex-wrap justify-around mt-40 p-4">
+      <div className="w-screen h-1/2 flex flex-wrap justify-around  p-4">
         <Outlet />
-        <div className="mt-20 text-primary ">
-          <ul className="flex items-center justify-around w-96 first:ml-[-8px]">
-            <li>
-              <Link to="/destination/moon">MOON</Link>
-            </li>
-            <li>
-              <Link to="/destination/mars">MARS</Link>
-            </li>
-            <li>
-              <Link to="/destination/europa">EUROPA</Link>
-            </li>
-            <li>
-              <Link to="/destination/titan">TITAN</Link>
-            </li>
+        <div className="mt-10 text-primary ">
+          <ul className="flex items-center justify-around w-96 h-10 first:ml-[-8px]">
+            {planetsData.map((planet) => {
+              const { name, link } = planet;
+
+              return (
+                <li
+                  key={planet.id}
+                  className="hover:border-b-2 border-white h-10"
+                >
+                  <Link to={link}>{name.toUpperCase()}</Link>
+                </li>
+              );
+            })}
           </ul>
           <div>
             <h1 className="text-8xl font-Bellefair leading-normal pl-2">
---
+++ planetsData.js
@@ -0,0 +1,22 @@
+export const planetsData = [
+    {
+        id:1,
+        name: 'moon',
+        link: '/destination/moon'
+    },
+    {
+        id:2,
+        name: 'mars',
+        link: '/destination/mars'
+    },
+    {
+        id:3,
+        name: 'europa',
+        link: '/destination/europa'
+    },
+    {
+        id:4,
+        name: 'titan',
+        link: '/destination/titan'
+    }
+]
