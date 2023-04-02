27ab4f23fb0bec13d877b1824cb87c31a304df49
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2023-04-02 15:29:46 +0100

    Progress on fixes for the destination page

--- app/routes/destination/index.tsx
+++ app/routes/destination/index.tsx
@@ -19,19 +19,6 @@ export const loader: LoaderFunction = async () => {
 
   const planet = await res.json();
 
-  return planet;
-};
-
-export default function DestinationIndex() {
-  const [planetId, setPlanetId] = useState(1);
-  const { width } = useWindowDimensions();
-
-  const planet = useLoaderData();
-
-  // console.log(planet);
-
-  const sm = 640;
-
   const planetsData: any = [
     {
       id: 1,
@@ -71,6 +58,19 @@ export default function DestinationIndex() {
     },
   ];
 
+  return { planet, planetsData };
+};
+
+export default function DestinationIndex() {
+  const [planetId, setPlanetId] = useState(1);
+  const { width } = useWindowDimensions();
+
+  const { planet, planetsData } = useLoaderData();
+
+  console.log({ planet, planetsData });
+
+  const sm = 640;
+
   const handleSelection = (planetId: number) => {
     setPlanetId(planetId);
   };
@@ -107,23 +107,33 @@ export default function DestinationIndex() {
         <div className="place-items-center sm:p-28">
           <div className="grid grid-cols-1 place-content-center p-5 md:w-[573px] lg:h-[472px] lg:w-[444px]">
             <ul className="flex justify-around items-center place-self-center m-auto sm:m-0 gap-4 w-[237.5px] sm:w-[330px] lg:place-self-start">
-              {planetsData.map((planet: { id: number; name: string }) => (
-                <li key={planet.id} className="hover:border-b-2 h-10 text-base">
-                  <button onClick={() => handleSelection(planet.id)}>
-                    {planet.name.toUpperCase()}
-                  </button>
-                </li>
-              ))}
+              {planetsData.map((planet: { id: number; name: string }) => {
+                return (
+                  <li
+                    key={planet.id}
+                    className="hover:border-b-2 h-10 text-base"
+                  >
+                    <button onClick={() => handleSelection(planet.id)}>
+                      {planet.name.toUpperCase()}
+                    </button>
+                  </li>
+                );
+              })}
             </ul>
             <div className="py-5 text-center lg:text-left h-72">
-              <Fragment key={planet.id}>
-                <h2 className="text-[56px] font-Bellefair md:text-[80px] lg:text-8xl">
-                  {planet.englishName.toUpperCase()}
-                </h2>
-                <p className="text-center lg:text-left font-Barlow">
-                  {planet.name}
-                </p>
-              </Fragment>
+              {planetsData.map(
+                (planet: { id: number; name: string; caption: string }) =>
+                  planetId === planet.id ? (
+                    <Fragment key={planet.id}>
+                      <h2 className="text-[56px] font-Bellefair md:text-[80px] lg:text-8xl">
+                        {planet.name.toUpperCase()}
+                      </h2>
+                      <p className="text-center lg:text-left font-Barlow">
+                        {planet.caption}
+                      </p>
+                    </Fragment>
+                  ) : null
+              )}
             </div>
             <hr className="opacity-20 p-3 md:w-[535px]  lg:w-[410px]" />
             <div className="grid grid-cols-2 text-center md:w-[535px] lg:w-[444px] lg:grid-cols-2">
